9c0007bdf5c11791029c02176035a5a37a448523
Jan Valentik <jvalentik@sk.ibm.com>
2017-09-16 13:08:38 +0200

    Initial commit

---
+++ app/src/main/java/com/cloudis/dinnerdecider/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.cloudis.dinnerdecider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private val foodList = arrayListOf("Chinese", "McDonalds", "Pizza", "Slovak")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            foodList.add(addFoodTxt.text.toString())
+        }
+    }
+}
